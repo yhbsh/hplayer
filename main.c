2f8a899598c906cce82d6e2ea671313900f08726
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-12-24 02:39:16 +0300

    Read & Decode videos frame by frame

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <stdio.h>
+
+int main(void) {
+    AVFormatContext *avformat_context = NULL;
+    avformat_open_input(&avformat_context, "file.mp4", NULL, NULL);
+    avformat_find_stream_info(avformat_context, NULL);
+
+    AVStream *av_stream = avformat_context->streams[0];
+    AVCodecParameters *avcodec_parameters = av_stream->codecpar;
+    enum AVCodecID avcodec_id = avcodec_parameters->codec_id;
+
+    const AVCodec *avcodec = avcodec_find_decoder(avcodec_id);
+    AVCodecContext *avcodec_context = avcodec_alloc_context3(avcodec);
+    avcodec_parameters_to_context(avcodec_context, avcodec_parameters);
+    avcodec_open2(avcodec_context, avcodec, NULL);
+
+    AVFrame *av_frame = av_frame_alloc();
+    AVPacket av_packet;
+    while (av_read_frame(avformat_context, &av_packet) >= 0) {
+        avcodec_send_packet(avcodec_context, &av_packet);
+        while (avcodec_receive_frame(avcodec_context, av_frame) == 0) {
+            printf("%d\n", *av_frame->linesize);
+        }
+        av_packet_unref(&av_packet);
+    }
+
+    av_frame_free(&av_frame);
+    avcodec_free_context(&avcodec_context);
+    avformat_close_input(&avformat_context);
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,14 @@
+clean: run
+	rm a.out
+
+
+run: build
+	./a.out
+
+debug:
+	mkdir -p build
+	clang -I/opt/homebrew/include -g3 -o build/main main.c -L/opt/homebrew/lib -lavformat -lavcodec
+
+
+build: main.c
+	clang -Wall -Wextra -pedantic -I/opt/homebrew/include -O3 main.c -L/opt/homebrew/lib -lavformat -lavcodec
