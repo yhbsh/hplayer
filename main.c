926e7a59f47e6bda249d6c5cef697e6302ee701b
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-12-24 02:45:58 +0300

    Make immutable struct pointers const

--- main.c
+++ main.c
@@ -4,24 +4,30 @@
 
 int main(void) {
     AVFormatContext *avformat_context = NULL;
+
     avformat_open_input(&avformat_context, "file.mp4", NULL, NULL);
     avformat_find_stream_info(avformat_context, NULL);
 
-    AVStream *av_stream = avformat_context->streams[0];
-    AVCodecParameters *avcodec_parameters = av_stream->codecpar;
+    const AVStream *av_stream = avformat_context->streams[0];
+    const AVCodecParameters *avcodec_parameters = av_stream->codecpar;
     enum AVCodecID avcodec_id = avcodec_parameters->codec_id;
 
     const AVCodec *avcodec = avcodec_find_decoder(avcodec_id);
     AVCodecContext *avcodec_context = avcodec_alloc_context3(avcodec);
+
     avcodec_parameters_to_context(avcodec_context, avcodec_parameters);
     avcodec_open2(avcodec_context, avcodec, NULL);
 
     AVFrame *av_frame = av_frame_alloc();
     AVPacket av_packet;
-    while (av_read_frame(avformat_context, &av_packet) >= 0) {
+    while (1) {
+
+        if (av_read_frame(avformat_context, &av_packet) < 0)
+            break;
+
         avcodec_send_packet(avcodec_context, &av_packet);
         while (avcodec_receive_frame(avcodec_context, av_frame) == 0) {
-            printf("%d\n", *av_frame->linesize);
+            // printf("%d\n", *av_frame->linesize);
         }
         av_packet_unref(&av_packet);
     }
