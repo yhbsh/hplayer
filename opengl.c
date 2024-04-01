086bc20eaf96a3db1247e0df0977c92a30f54687
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-04-01 15:39:14 +0300

    Improve opengl implementation

--- opengl.c
+++ opengl.c
@@ -1,164 +1,156 @@
-#define GL_SILENCE_DEPRECATION
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-#include <libswscale/swscale.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-bool load_frame(const char *fp, int *w, int *h, unsigned char **buff) {
-  int ret = 0;
+const char *vs =
+    "#version 410\n"
+    "layout (location = 0) in vec4 position;\n"
+    "layout (location = 1) in vec2 texCoord;\n"
+    "out vec2 texCoordVarying;\n"
+    "void main() {\n"
+    "    gl_Position = position;\n"
+    "    texCoordVarying = texCoord;\n"
+    "}\n";
 
-  AVFormatContext *avformat_ctx = NULL;
-  ret = avformat_open_input(&avformat_ctx, fp, NULL, NULL);
-  if (ret != 0) {
-    fprintf(stderr, "[ERROR]: avformat_open_input: %s\n", av_err2str(ret));
-    return false;
-  }
+const char *fs =
+    "#version 410\n"
+    "in vec2 texCoordVarying;\n"
+    "uniform sampler2D textureY;\n"
+    "uniform sampler2D textureU;\n"
+    "uniform sampler2D textureV;\n"
+    "out vec4 fragColor;\n"
+    "void main() {\n"
+    "    vec3 yuv;\n"
+    "    vec3 rgb;\n"
+    "    yuv.x = texture(textureY, texCoordVarying).r;\n"
+    "    yuv.y = texture(textureU, texCoordVarying).r - 0.5;\n"
+    "    yuv.z = texture(textureV, texCoordVarying).r - 0.5;\n"
+    "    rgb = mat3(1.0, 1.0, 1.0, 0.0, -0.344, 1.772, 1.402, -0.714, 0.0) * yuv;\n"
+    "    fragColor = vec4(rgb, 1.0);\n"
+    "}\n";
 
-  const AVCodec *avcodec;
-  ret = av_find_best_stream(avformat_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &avcodec, 0);
-  if (ret < 0 || avcodec == NULL) {
-    fprintf(stderr, "[ERROR]: av_find_best_stream: %s\n", av_err2str(ret));
-    return false;
-  }
-
-  AVStream *vs = avformat_ctx->streams[ret];
-  AVCodecParameters *avcodec_params = vs->codecpar;
-  AVCodecContext *avcodec_ctx = avcodec_alloc_context3(avcodec);
-  if (avcodec_ctx == NULL) {
-    fprintf(stderr, "[ERROR]: avcodec_alloc_context3 failed\n");
-    return false;
-  }
-
-  ret = avcodec_parameters_to_context(avcodec_ctx, avcodec_params);
-  if (ret < 0) {
-    fprintf(stderr, "[ERROR]: avcodec_parameters_to_context: %s\n", av_err2str(ret));
-    return false;
-  }
-
-  ret = avcodec_open2(avcodec_ctx, avcodec, NULL);
-  if (ret < 0) {
-    fprintf(stderr, "[ERROR]: avcodec_open2: %s\n", av_err2str(ret));
-    return false;
-  }
-
-  AVFrame *avframe = av_frame_alloc();
-  if (avframe == NULL) {
-    fprintf(stderr, "[ERROR]: av_frame_alloc\n");
-    return false;
-  }
-
-  AVPacket *avpacket = av_packet_alloc();
-  if (avpacket == NULL) {
-    fprintf(stderr, "[ERROR]: av_packet_alloc\n");
-    return false;
-  }
-
-  while (av_read_frame(avformat_ctx, avpacket) >= 0) {
-    if (avpacket->stream_index != vs->index) {
-      continue;
-    }
-
-    ret = avcodec_send_packet(avcodec_ctx, avpacket);
-    if (ret < 0) {
-      fprintf(stderr, "[ERROR]: avcodec_send_packet: %s\n", av_err2str(ret));
-      return false;
-    }
-
-    ret = avcodec_receive_frame(avcodec_ctx, avframe);
-    if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-      continue;
-    }
-    if (ret < 0) {
-      fprintf(stderr, "[ERROR]: avcodec_receive_frame: %s\n", av_err2str(ret));
-      return false;
-    }
-    av_packet_unref(avpacket);
-    break;
-  }
-
-  struct SwsContext *sws_scaler_ctx = sws_getContext(avframe->width, avframe->height, avcodec_ctx->pix_fmt, avframe->width, avframe->height, AV_PIX_FMT_RGB0, SWS_BILINEAR, NULL, NULL, NULL);
-
-  if (sws_scaler_ctx == NULL) {
-    fprintf(stderr, "[ERROR]: sws_getContext\n");
-    return false;
-  }
-
-  unsigned char *data = malloc(avframe->width * avframe->height * 4);
-  unsigned char *dest[4] = {data, NULL, NULL, NULL};
-  int dest_linesize[4] = {avframe->width * 4, 0, 0, 0};
-  sws_scale(sws_scaler_ctx, (const unsigned char *const *)avframe->data, avframe->linesize, 0, avframe->height, dest, dest_linesize);
-  sws_freeContext(sws_scaler_ctx);
-
-  *w = avframe->width;
-  *h = avframe->height;
-  *buff = data;
-
-  avformat_close_input(&avformat_ctx);
-  avformat_free_context(avformat_ctx);
-  av_frame_free(&avframe);
-  av_packet_free(&avpacket);
-  avcodec_free_context(&avcodec_ctx);
-
-  return true;
-}
-
-int main(void) {
-  const char *fp = "samples/s2.mp4";
-  int x, y;
-
-  unsigned char *buff;
-  if (!load_frame(fp, &x, &y, &buff)) {
-    fprintf(stderr, "[ERROR]: could not load video frame\n");
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "[USAGE]: ./main [url]");
     return 1;
   }
 
+  int ret;
+  const int width = 960;
+  const int height = 540;
+
   glfwInit();
-  GLFWwindow *w = glfwCreateWindow(x, y, "Window", NULL, NULL);
-  glfwMakeContextCurrent(w);
-  printf("version = %s\n", glGetString(GL_VERSION));
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  GLFWwindow *window = glfwCreateWindow(width, height, "Animated UV Pattern", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  glewInit();
+  printf("renderer = %s\nversion = %s\n", glGetString(GL_RENDERER), glGetString(GL_VERSION));
 
-  unsigned int texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture);
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, buff);
+  AVFormatContext *format_context = NULL;
+  ret = avformat_open_input(&format_context, argv[1], NULL, NULL);
+  ret = avformat_find_stream_info(format_context, NULL);
+  AVStream *stream = format_context->streams[1];
+  enum AVCodecID codec_id = stream->codecpar->codec_id;
+  const AVCodec *codec = avcodec_find_decoder(codec_id);
+  AVCodecContext *codec_context = avcodec_alloc_context3(codec);
+  ret = avcodec_parameters_to_context(codec_context, stream->codecpar);
+  ret = avcodec_open2(codec_context, codec, NULL);
+  AVFrame *frame = av_frame_alloc();
+  AVPacket packet;
 
-  while (!glfwWindowShouldClose(w)) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vs, NULL);
+  glCompileShader(vertex_shader);
+  GLint status;
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &status);
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, x, y, 0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
+  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fs, NULL);
+  glCompileShader(fragment_shader);
 
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glBegin(GL_QUADS);
-    glTexCoord2d(0, 0);
-    glVertex2i(0, 0);
-    glTexCoord2d(1, 0);
-    glVertex2i(x, 0);
-    glTexCoord2d(1, 1);
-    glVertex2i(x, y);
-    glTexCoord2d(0, 1);
-    glVertex2i(0, y);
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+  glUseProgram(program);
 
-    glfwSwapBuffers(w);
-    glfwWaitEvents();
+  GLuint textureY, textureU, textureV;
+  glGenTextures(1, &textureY);
+  glGenTextures(1, &textureV);
+  glGenTextures(1, &textureU);
+
+  GLfloat vertices[] = {-1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f};
+  GLfloat texCoords[] = {0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f};
+
+  GLuint VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  GLuint VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(texCoords), NULL, GL_STATIC_DRAW);
+  glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+  glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(texCoords), texCoords);
+
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void *)sizeof(vertices));
+  glEnableVertexAttribArray(1);
+
+  while (!glfwWindowShouldClose(window)) {
+    ret = av_read_frame(format_context, &packet);
+    if (packet.stream_index != stream->index) {
+      continue;
+    }
+
+    ret = avcodec_send_packet(codec_context, &packet);
+    while (ret >= 0) {
+      ret = avcodec_receive_frame(codec_context, frame);
+      if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+        break;
+      }
+
+      glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      // upload to gpu
+      glActiveTexture(GL_TEXTURE0);
+      glBindTexture(GL_TEXTURE_2D, textureY);
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frame->width, frame->height, 0, GL_RED,
+                   GL_UNSIGNED_BYTE, frame->data[0]);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glUniform1i(glGetUniformLocation(program, "textureY"), 0);
+
+      glActiveTexture(GL_TEXTURE1);
+      glBindTexture(GL_TEXTURE_2D, textureU);
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frame->width / 2, frame->height / 2, 0, GL_RED,
+                   GL_UNSIGNED_BYTE, frame->data[1]);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glUniform1i(glGetUniformLocation(program, "textureU"), 1);
+
+      glActiveTexture(GL_TEXTURE2);
+      glBindTexture(GL_TEXTURE_2D, textureV);
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frame->width / 2, frame->height / 2, 0, GL_RED,
+                   GL_UNSIGNED_BYTE, frame->data[2]);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glUniform1i(glGetUniformLocation(program, "textureV"), 2);
+
+      glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+    }
+
+    av_packet_unref(&packet);
   }
 
   glfwTerminate();
-
   return 0;
 }
